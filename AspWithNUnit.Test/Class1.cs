a656d8a1b5bdb053e09b4e58f5ca82780d7bf2cd
srsave <58839130+srsave@users.noreply.github.com>
2021-03-16 09:54:56 +0530

    ok

--- AspWithNUnit.Test/Class1.cs
+++ AspWithNUnit.Test/Class1.cs
@@ -38,7 +38,7 @@ namespace SampleAddition1_NUnit
 
                 yield return new TetstSetInfo("1", "10");
                 yield return new TetstSetInfo("500", "1000");
-                yield return new TetstSetInfo("50.70", "Text");
+                yield return new TetstSetInfo("50.70", "55");
             }
         }
         private static IEnumerable<TetstSetInfo> ParamterValuesSet2
@@ -49,7 +49,7 @@ namespace SampleAddition1_NUnit
                 yield return new TetstSetInfo("", "10");
                 yield return new TetstSetInfo("500", "");
                 yield return new TetstSetInfo("NN", "10.800");
-                yield return new TetstSetInfo("10.800", "Sudeep");
+                yield return new TetstSetInfo("10.800", "155");
             }
         }
 
